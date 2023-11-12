daf838ffd1efc445416c510828bb8de7015754de
hiroyuki <11hi01ro30yuki@gmail.com>
2023-11-12 16:59:06 +0900

    taxメソッドの計算式を修正

--- src/Review01.java
+++ src/Review01.java
@@ -2,15 +2,18 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        //
+        //価格と税率を設定
         int price = 1500;
-        int tax = 10;
-        
+        double tax = 0.1;
+
+        //計算結果を出力
         System.out.println(price + "円の商品の税込価格は" + (tax(price, tax) + price) + "円" + "（消費税は" + tax(price, tax) + "円）です。");
     }
-    
-    public static int tax(int price, int tax) {
-        return price / tax;
+
+    //価格x税率を計算
+    public static int tax(int price, double tax) {
+        double result = price * tax;
+        return (int)result;
     }
 
 }
